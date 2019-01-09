6fb5a70c5a6496e54614b939e1d43b392c526bfa
hatemodify <hatemodify@gmail.com>
2019-01-09 18:55:08 +0900

    review update

---
+++ client/src/components/layout/Gnb.vue
@@ -0,0 +1,38 @@
+<template>
+  <header id="site_header">
+    <div class="header_inner">
+      <h1 class="site_logo">
+        <router-link to="/">
+          <img src="@/assets/images/logo.png" widt="100" alt>
+        </router-link>
+      </h1>
+      <nav class="site_menu">
+        <ul class="list_menu">
+          <li>
+            <router-link to>내주변</router-link>
+          </li>
+          <li>
+            <router-link to>지역</router-link>
+          </li>
+          <li>
+            <router-link to>기획전</router-link>
+          </li>
+        </ul>
+      </nav>
+      <div class="wrap_search">
+        <button class="btn_search"></button>
+      </div>
+      <div class="wrap_util">
+        <router-link to class="link_util">회원가입</router-link>
+        <router-link to class="link_util">로그인</router-link>
+      </div>
+    </div>
+  </header>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
---
+++ client/src/views/main.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="wrap">
+    <p>Lat = {{lat}} Lon ={{lon}}</p>
+    <p>{{error}}</p>
+    <button @click="myFunction()">Click Me</button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      error: "",
+      lat: "",
+      lon: ""
+    };
+  },
+  created() {
+    if (!navigator.geolocation) {
+      console.log(`용자의 브라우저는 지오로케이션을 지원하지 않습니다.`);
+      return;
+    }
+  },
+  methods: {
+    myFunction: function() {
+      console.log("asdasd");
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(this.showPosition);
+      } else {
+        this.error = "Geolocation is not supported.";
+      }
+    },
+    showPosition: function(position) {
+      this.lat = position.coords.latitude;
+      this.lon = position.coords.longitude;
+    }
+  }
+};
+</script>
