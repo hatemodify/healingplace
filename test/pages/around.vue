ded9820fc1d742d1fbd099b292a8bdd8c5061d7a
hatemodify <hatemodify@gmail.com>
2019-04-04 01:22:09 +0900

    nuxt 적용 테스트

---
+++ test/pages/around.vue
@@ -0,0 +1,42 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      coords: {}
+    }
+  },
+  created() {
+    this.geolocate()
+    console.log(this.coords)
+  },
+  methods: {
+    geolocate() {
+      navigator.geolocation.getCurrentPosition(
+        this.onGeolocateSuccess,
+        this.onGeolocateError
+      )
+    },
+    onGeolocateSuccess(coordinates) {
+      const coords = {
+        latitude: coordinates.coords.latitude,
+        longitude: coordinates.coords.longitude
+      }
+      this.coords = coords
+    },
+
+    onGeolocateError(error) {
+      if (error.code === 1) {
+        // they said no
+      } else if (error.code === 2) {
+        // position unavailable
+      } else if (error.code === 3) {
+        // timeout
+      }
+    }
+  }
+}
+</script>
